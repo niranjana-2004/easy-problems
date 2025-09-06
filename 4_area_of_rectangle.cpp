b70b22db890b70c18479b3376ef0897d4ef76438
niranjana-2004 <niranjana051104@gmail.com>
2025-09-06 13:25:24 +0530

    qn_4_done

---
+++ 4_area_of_rectangle.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int x,y,area;
+
+    cout<<"Enter the length:";
+    cin>>x;
+
+    cout<<"Enter the breadth:";
+    cin>>y;
+
+    area=x*y;
+    cout<<"Area of rectangle: "<<area<<endl;
+
+    return 0;
+}
