7bdb81ce8fa1d3f957646ba335803f07b60abb83
niranjana-2004 <niranjana051104@gmail.com>
2025-09-06 13:16:28 +0530

    qn_3_done

---
+++ 3_print_namah_shivaya.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    string name;
+
+    cout<<"Enter your name:";
+    cin>>name;
+
+    cout<<"Namah Shivaya! "<<name<<endl;
+    return 0;
+}
